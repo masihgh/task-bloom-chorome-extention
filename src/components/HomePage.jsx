16d3ed9fc8faf8d9e70d886f5606948170d3b6b3
MasihGh <masihprogrammer@gmail.com>
2025-01-21 01:03:23 +0330

    initialize clean Vite + React Chrome extension setup 🦖

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Hello T-rex!! 🦖</h1>
+      <p>This is a homepage built with React and Vite For TaskBloom.</p>
+    </div>
+  );
+}
+
+export default HomePage;
